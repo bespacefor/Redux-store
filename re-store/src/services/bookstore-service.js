44daa19d546fd2e45e692ddb3802e5371b568d10
Sviatlana Lukyantsava <Sviatlana_Lukyantsava@epam.com>
2022-12-01 22:42:16 +0300

    feat: connecting components

--- re-store/src/services/bookstore-service.js
+++ re-store/src/services/bookstore-service.js
@@ -1,7 +1,18 @@
 export default class BookstoreService {
 
     getBooks() {
-        return [];
+        return [
+            {
+                id: 1,
+                title: 'Test test test',
+                author: 'Name name name'
+            },
+            {
+                id: 2,
+                title: 'Test test test 2',
+                author: 'Name name name 2'
+            },
+        ];
     }
 
 }
