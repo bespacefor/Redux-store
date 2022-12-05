613c5968107c36abeba47f072ed012b115c6a2b5
Sviatlana Lukyantsava <Sviatlana_Lukyantsava@epam.com>
2022-12-05 23:34:53 +0300

    feat: update routing

--- re-store/src/components/pages/home-page.js
+++ re-store/src/components/pages/home-page.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import BookList from '../book-list/book-list';
 
 const HomePage = () => {
+    const books = [
+        {
+            id: 1,
+            title: 'Test test test',
+            author: 'Name name name'
+        },
+        {
+            id: 2,
+            title: 'Test test test 2',
+            author: 'Name name name 2'
+        },
+    ];
+    
     return (
-        <div>HomePage</div>
+        <BookList books={books} />
     );
 };
 
