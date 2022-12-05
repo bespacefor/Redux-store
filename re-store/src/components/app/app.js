65e4cef20ace5b9ec3ceafd08b4cb230be593567
Sviatlana Lukyantsava <Sviatlana_Lukyantsava@epam.com>
2022-12-06 00:55:49 +0300

    feat: add data and styles

--- re-store/src/components/app/app.js
+++ re-store/src/components/app/app.js
@@ -5,17 +5,20 @@ import './app.css';
 
 const App = () => {
     return (
-        <Routes>
-            <Route
-                path='/'
-                element={<HomePage/>}
-                exact
-            />
-            <Route
-                path='/cart'
-                element={<CartPage/>}
-            />
-        </Routes>
+        <main role='main' className='container'>
+            <Routes>
+                <Route
+                    path='/'
+                    element={<HomePage />}
+                    exact
+                />
+                <Route
+                    path='/cart'
+                    element={<CartPage />}
+                />
+            </Routes>
+
+        </main>
     );
 };
 
