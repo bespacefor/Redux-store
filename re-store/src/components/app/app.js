e126750b993ff903e7d1dcc8eb61d4a4b5651ed4
Sviatlana Lukyantsava <Sviatlana_Lukyantsava@epam.com>
2022-12-05 22:12:19 +0300

    Revert "feat: add basic routing"
    This reverts commit e137e943b6e4dae6398611293c06cd87e7ac6608.

--- re-store/src/components/app/app.js
+++ re-store/src/components/app/app.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { HomePage, CartPage } from '../pages';
 import './app.css';
+import { withBookstoreService } from '../hoc';
 
-const App = () => {
-    return (
-        <Routes>
-            <Route
-                path='/'
-                element={<HomePage/>}
-                exact
-            />
-            <Route
-                path='/cart'
-                element={<CartPage/>}
-            />
-        </Routes>
-    );
+const App = ({ bookstoreService }) => {
+    console.log(bookstoreService.getBooks());
+    return <div>App</div>;
 };
 
-export default App;
+export default withBookstoreService()(App);
