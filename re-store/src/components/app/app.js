33765425f7654dd3505ccbbf1a73473fcf83c7f4
Sviatlana Lukyantsava <Sviatlana_Lukyantsava@epam.com>
2022-12-06 01:32:48 +0300

    feat: add header and shopping cart table

--- re-store/src/components/app/app.js
+++ re-store/src/components/app/app.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import ShopHeader from '../shop-header';
 import { HomePage, CartPage } from '../pages';
 import './app.css';
 
 const App = () => {
     return (
         <main role='main' className='container'>
+            <ShopHeader numItems={5} total={210}/>
             <Routes>
                 <Route
                     path='/'
@@ -17,7 +19,6 @@ const App = () => {
                     element={<CartPage />}
                 />
             </Routes>
-
         </main>
     );
 };
