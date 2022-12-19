a0432732578eeee298649e119ee8b379c0bbca7e
Sviatlana Lukyantsava <Sviatlana_Lukyantsava@epam.com>
2022-12-19 23:30:01 +0300

    feat: add thunk

--- re-store/src/store.js
+++ re-store/src/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 
 import reducer from './reducers';
 
@@ -18,8 +19,15 @@ const stringMiddleware = () => (next) => (action) => {
     return next(action);
 };
 
-const store = createStore(reducer, applyMiddleware(stringMiddleware, logMiddleware));
+const store = createStore(reducer, applyMiddleware(
+    thunkMiddleware, stringMiddleware, logMiddleware));
 
-store.dispatch('HELLO_WORLD');
+const delayedActionCreator = (timeout) => (dispatch) => {
+    setTimeout(() => dispatch({
+        type: 'DELAYED_ACTION'
+    }), timeout);
+};
+
+store.dispatch(delayedActionCreator(2000));
 
 export default store;
